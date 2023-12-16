27ef89b82e985ce91544b080789c024c0cf3c323
Bilychuk <bilychukkateryna@ukr.net>
2023-12-16 14:49:43 +0200

    add task1

---
+++ js/lesson1.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Task 1
+//Використовуя функцію if...else,
+//напишіть код, який буде питати:
+//"Яка офіційна назва JavaScript?"
+//Якщо користувач вводить "ECMAScript",
+//то показати через alert: "Вірно!"
+//в противному випадку відобразити:"Не знаєте? ECMAScript!"
+
+const question = prompt("Яка офіційна назва JavaScript?");
+// if (question === "ECMAScript") {
+//   alert("Вірно!")
+// } else {
+//     alert("Не знаєте? ECMAScript!");
+// }
+alert(question === "ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!");
