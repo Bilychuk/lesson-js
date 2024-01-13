19c1d028495701572785df21a37bd3a2fdf21b8e
OlikKorsun <145294542+OlikKorsun@users.noreply.github.com>
2024-01-13 15:14:23 +0200

    add task 2-2

--- js/lesson2.js
+++ js/lesson2.js
@@ -6,11 +6,31 @@
 //Видаліть перший елемент масива та виведіть його в консоль
 // Додайте 'Реп' та 'Реггі' на початок списку.
 
-const styles = ['Джаз', 'Блюз'];
-styles.push('Рок-н-ролл');
-const index = styles.indexOf('Блюз');
-// styles[index] = 'Класика';
-styles.splice(index, 1, 'Класика');
-console.log(styles.shift());
-styles.unshift('Реп', 'Реггі');
-console.log(styles);
+// const styles = ['Джаз', 'Блюз'];
+// styles.push('Рок-н-ролл');
+// const index = styles.indexOf('Блюз');
+// // styles[index] = 'Класика';
+// styles.splice(index, 1, 'Класика');
+// console.log(styles.shift());
+// styles.unshift('Реп', 'Реггі');
+// console.log(styles);
+
+
+// Напиши функцію, яка перебирає масив логінів і перевіряє
+// чи є ім'я введене в інпут у цьому масиві і у разі,
+// якщо є - виводить повідомлення "Доступ дозволено"
+// в іншому випадку - "Користувач не знайден"
+
+const logins = ["Peter", "John", "Igor", "Sasha"];
+
+function checkLog(arrayNew) {
+    const login = prompt("Enter your login!");
+        if (arrayNew.includes(login)) {
+            console.log("Доступ дозволено");
+            return;
+        }
+    console.log("Користувач не знайден")
+        
+}
+
+checkLog(logins);
