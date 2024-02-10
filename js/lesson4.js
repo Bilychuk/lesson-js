419493e5f86e778aa2d6dfe1e180a60e8c40756f
Ekaterina-Skrinnikova <kati.skrinnikova@gmail.com>
2024-02-10 17:20:54 +0200

    taski

--- js/lesson4.js
+++ js/lesson4.js
@@ -131,3 +131,83 @@ function onFormSubmit(event) {
   formLogin.elements.email.setAttribute("readonly", true);
   formLogin.elements.password.setAttribute("readonly", true);
 }
+
+// Task1;
+// Створити невелику гру
+// Спочатку на екрані з'являється якась фігура рандомного коліру в рандомному місті
+// Натискаючі на фігуру, вона змінює свою форму, колір, місце розташування
+
+// const forms = [
+//   "width: 100px; height: 100px; border-width: 1px;",
+//   "width: 100px; height: 100px; border-radius: 50%;",
+//   "width: 150px; height: 100px; border-width: 1px;",
+//   "width: 200px; height: 100px; border-radius: 100px / 50px; ",
+//   "width: 150px; height: 100px; transform: skew(20deg)",
+// ];
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// function randomither(max) {
+//   return Math.floor(Math.random() * max);
+// }
+
+// Task2;
+// Створити червоний квадрат розміром 50 на 50 рх
+// Додати кнопку "Зменшити", яка робить квадрат менше на 10 пікселів
+// Додати кнопку "Збільшити", яка робить його більше на 10 пікселів.
+
+// Task3;
+// <!-- ЗАДАЧА 6 -->
+//     <!-- <div>
+//       <p class="taskTitle">ЗАДАЧА 6</p>
+//       <ul class="list">
+//         <li class="listItem">1</li>
+//         <li class="listItem">4</li>
+//         <li class="listItem">8</li>
+//         <li class="listItem">16</li>
+//         <li class="listItem">20</li>
+//         <li class="listItem">30</li>
+//       </ul>
+//       <button id="double">Подвоїти</button>
+//     </div> -->
+//TODO:==============================================
+/*
+Завдання 6
+Натиснувши кнопку "Подвоювати", збільшити значення
+у кожному елементі списку у 2 рази
+*/
+
+// Task4
+// <!-- ЗАДАЧА 7 -->
+//     <div>
+//       <p class="taskTitle">ЗАДАЧА 7</p>
+//       <div class="outerCircle">
+//         <div class="innerCircle"></div>
+//       </div>
+//     </div>
+/*
+Завдання 7
+При натисканні на коло він повинен слідувати за курсором.
+При повторному натисканні він стає в початкове положення.
+*/
+
+// Task5
+
+// <!-- ЗАДАЧА 11 -->
+//     <!-- <p class="taskTitle">ЗАДАЧА 11</p>
+//     <input class="contactsFilter" type="text" />
+//     <ul class="contacts">
+//       <li class="contact">Peter Miller</li>
+//       <li class="contact">John Carter</li>
+//       <li class="contact">Amanda Diaz</li>
+//       <li class="contact">Brad Sanders</li>
+//       <li class="contact">Devis Brooks</li>
+//       <li class="contact">Robert Morgan</li>
+//     </ul> -->
+//TODO:==============================================
+/*
+Завдання 11
+Наведено список людей. Зроби фільтр на ім'я/прізвище.
+*/
